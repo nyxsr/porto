00b3d8d7822bf8432374fe504bf103397aeeaee4
Sahrul Ramdan <sahrulramdan.75@gmail.com>
2025-05-29 01:04:23 +0700

    chore: setup prettier, eslint and core dependencies

---
+++ src/constants/seo.ts
@@ -0,0 +1,10 @@
+export const SITE = {
+  NAME: "nyxsr",
+  DESCRIPTION:
+    "I love build an app that help people, each process, each step that i took to make it happen are a part of beautiness of the app. I'll help you to build your dream project.",
+} as const;
+
+export const TITLE = {
+  DEFAULT: `${SITE.NAME} | Portfolio`,
+  HOME: `${SITE.NAME} | Portfolio`,
+}
